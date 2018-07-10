09e5532e8d7734bade521fd53d5768286323d7cc
Savannac <savannavchada@gmail.com>
2018-07-10 15:58:05 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -83,10 +83,10 @@ puts proper_sentence
 
 
 # Challenge 7: String Switcheroo
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub ("A","O")
+sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+sentence = sentence. gsub ("A","O").gsub ("a","o") # Add your gsub chain here
 
 puts sentence
